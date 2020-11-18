218d97be37a5a0cb9bf11940ebae687e3d45af70
kevinkim910408 <kevinkim0408@naver.com>
2020-11-18 18:23:00 +0900

    Title Scene - Buttons, BGM, BG
    Title Scene - Buttons, BGM, BG

---
+++ Assets/00_Scripts/00_UI/TitleButtons.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: TitleButtons.cs
+/// Date last Modified: 2020-11-18
+/// Program description
+///  - Managing start and instructor buttons 
+///  
+/// Revision History
+/// 2020-11-18: Added functions to start and instructor buttons  
+/// 
+/// </summary>
+
+public class TitleButtons : MonoBehaviour
+{
+    // scene name
+    [Header("Scene Names")]
+    public string startScene;
+    public string InstructorScene;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onStart()
+    {
+        SceneManager.LoadScene(startScene);
+    }
+    public void onInstructor()
+    {
+        SceneManager.LoadScene(InstructorScene);
+    }
+}
