c41ace6082de72f6d8df8f6b7ef32139ef97e5b5
kevinkim910408 <kevinkim0408@naver.com>
2020-11-24 22:34:56 +0900

    Simple Ingame Instructor 01 - Jump
    Simple Ingame Instructor 01 - Jump

---
+++ Assets/00_Scripts/00_UI/GuidePanel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidePanel : MonoBehaviour
+{
+    public GameObject guidePanel;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        guidePanel.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Time.timeScale = 0.0f;
+            guidePanel.SetActive(true);
+        }
+    }
+
+    public void OnReady()
+    {
+        Time.timeScale = 1.0f;
+        guidePanel.SetActive(false);
+        Destroy(gameObject);
+    }
+}
--- Assets/00_Scripts/PlatformMovingLR.cs
+++ Assets/00_Scripts/PlatformMovingLR.cs
@@ -2,6 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 
+/// Name: Junho Kim
+/// Student#: 101136986
+/// The Source file name: PlatformMovingLR.cs
+/// Date last Modified: 2020-11-24
+/// Program description
+///  - platforms move left to right and right to left
+///  
+/// Revision History
+/// 2020-11-24: Added functions of moving
+/// 
+/// </summary>
+
 public class PlatformMovingLR : MonoBehaviour
 {
     public float xPosition01;
